cfe8ea1c5fa3c525be604bde1c51b7d56fbd9135
Michael Burkat <mike.burkat@gmail.com>
2015-02-24 14:02:47 -0500

    added a connection pool

---
+++ redis/pool.go
@@ -0,0 +1,90 @@
+package redis
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Pool struct {
+	network     string
+	address     string
+	connections []*Conn
+	free        []*Conn
+	sync.Mutex
+}
+
+func NewPool(numConn int, network, address string) (*Pool, error) {
+	pool := Pool{network: network, address: address, connections: []*Conn{}, free: []*Conn{}}
+	for i := 0; i < numConn; i++ {
+		pool.Lock()
+		if err := pool.add(); err != nil {
+			return nil, err
+		}
+		pool.Unlock()
+	}
+	return &pool, nil
+}
+
+func (pool *Pool) Do(command string, args ...interface{}) (interface{}, error) {
+
+	if conn, err := pool.get(); err != nil {
+		return nil, err
+	} else {
+		reply, err := conn.Do(command, args...)
+		pool.release(conn)
+		return reply, err
+	}
+}
+
+func (pool *Pool) Close() {
+	for _, conn := range pool.connections {
+		conn.Close()
+	}
+}
+
+func (pool *Pool) PrintState() {
+	pool.Lock()
+	numConn := len(pool.connections)
+	numFree := len(pool.free)
+	pool.Unlock()
+	fmt.Println("conn:", numConn, "free:", numFree)
+}
+
+func (pool *Pool) get() (*Conn, error) {
+	pool.Lock()
+	if len(pool.free) == 0 {
+		if err := pool.add(); err != nil {
+			return nil, err
+		}
+	}
+	conn := pool.free[0]
+	pool.free = pool.free[1:]
+	pool.Unlock()
+	return conn, nil
+}
+
+func (pool *Pool) release(conn *Conn) {
+	pool.Lock()
+	if len(pool.free) > (len(pool.connections)/2)+1 {
+		for i, c := range pool.connections {
+			if c == conn {
+				pool.connections = append(pool.connections[:i], pool.connections[i+1:]...)
+				break
+			}
+		}
+		conn.Close()
+	} else {
+		pool.free = append(pool.free, conn)
+	}
+	pool.Unlock()
+}
+
+func (pool *Pool) add() error {
+	conn, err := Dial(pool.network, pool.address)
+	if err != nil {
+		return err
+	}
+	pool.connections = append(pool.connections, &conn)
+	pool.free = append(pool.free, &conn)
+	return nil
+}
---
+++ redis/pool_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2014 Datacratic. All rights reserved.
+
+package redis
+
+import (
+	"fmt"
+	"testing"
+	//	"time"
+)
+
+func TestPool(t *testing.T) {
+
+	redis, err := New(map[string]string{"port": "6366"})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	pool, err := NewPool(0, "tcp", "127.0.0.1:6366")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	pool.PrintState()
+
+	reply, err := pool.Do("SET", "one", "1")
+	if err != nil {
+		t.Error(err)
+	}
+
+	reply, err = pool.Do("GET", "one")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("%v\n", reply)
+
+	pool.PrintState()
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			reply, err = pool.Do("GET", "one")
+			if err != nil {
+				t.Error(err)
+			}
+			fmt.Printf("%v\n", reply)
+			pool.PrintState()
+		}()
+	}
+
+	pool.PrintState()
+	redis.Close()
+}
