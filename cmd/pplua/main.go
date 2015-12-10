b0c6302c95a7f3d73e1a469f370b843d02642cc1
Eric Robert <eric@datacratic.com>
2015-12-10 15:22:20 -0500

    added pplua

---
+++ cmd/pplua/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var include = regexp.MustCompile(`^\s*\#include\s"([^\"]*)"$`)
+
+func process(w io.Writer, filename string) (err error) {
+	if !strings.HasSuffix(filename, ".lua") {
+		filename += ".lua"
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+
+	defer file.Close()
+	s := bufio.NewScanner(bufio.NewReader(file))
+
+	for s.Scan() {
+		matches := include.FindStringSubmatch(s.Text())
+		if len(matches) == 2 {
+			if err = process(w, matches[1]); err != nil {
+				return
+			}
+		} else {
+			fmt.Fprintf(w, "%s\n", s.Text())
+		}
+	}
+
+	err = s.Err()
+	return
+}
+
+func main() {
+	out := flag.String("o", "", "name of the output file")
+	pkg := flag.String("p", "", "package name")
+
+	flag.Parse()
+
+	if *out == "" {
+		log.Fatal("missing output filename")
+	}
+
+	if *pkg == "" {
+		log.Fatal("missing package")
+	}
+
+	file, err := os.Create(*out)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	defer w.Flush()
+
+	fmt.Fprintf(w, "package %s\n", *pkg)
+
+	for _, item := range flag.Args() {
+		fmt.Fprintf(w, "\nvar %s = `\n", item)
+
+		err = process(w, item)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Fprintf(w, "`\n")
+	}
+}
