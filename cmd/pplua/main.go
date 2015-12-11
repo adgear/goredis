efd073c87bd3b8dac44c456e73db199f8248c76e
Eric Robert <eric@datacratic.com>
2015-12-11 10:41:57 -0500

    using a map for scripts

--- cmd/pplua/main.go
+++ cmd/pplua/main.go
@@ -44,6 +44,7 @@ func process(w io.Writer, filename string) (err error) {
 func main() {
 	out := flag.String("o", "", "name of the output file")
 	pkg := flag.String("p", "", "package name")
+	key := flag.String("v", "", "variable name")
 
 	flag.Parse()
 
@@ -53,6 +54,10 @@ func main() {
 
 	if *pkg == "" {
 		log.Fatal("missing package")
+	}
+
+	if *key == "" {
+		log.Fatal("missing variable name")
 	}
 
 	file, err := os.Create(*out)
@@ -65,16 +70,18 @@ func main() {
 	w := bufio.NewWriter(file)
 	defer w.Flush()
 
-	fmt.Fprintf(w, "package %s\n", *pkg)
+	fmt.Fprintf(w, "package %s\n\nvar %s = map[string]string{", *pkg, *key)
 
 	for _, item := range flag.Args() {
-		fmt.Fprintf(w, "\nvar %s = `\n", item)
+		fmt.Fprintf(w, "\n\t\"%s\": `\n", item)
 
 		err = process(w, item)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Fprintf(w, "`\n")
+		fmt.Fprintf(w, "`,\n")
 	}
+
+	fmt.Fprintf(w, "}\n")
 }
